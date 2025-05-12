95e8777a50b35f0263922780d9f1cb227175a363
Daniel Zavaleta <daniel.zavaleta.pe@cloudappi.net>
2025-05-11 19:14:29 -0500

    Nuevos cambios

--- src/main/java/com/commets/micro/service/commets_micro_service/Aplicacion/puertos/in/dto/CommentDto.java
+++ src/main/java/com/commets/micro/service/commets_micro_service/Aplicacion/puertos/in/dto/CommentDto.java
@@ -1,30 +1,37 @@
 package com.commets.micro.service.commets_micro_service.Aplicacion.puertos.in.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public class CommentDto {
-    @NotNull
-    private Long id_usuario;
 
+    @JsonProperty("id_usuario")
     @NotNull
-    private Long id_docente;
+    private Long idUsuario;
 
+    @JsonProperty("id_docente")
+    @NotNull
+    private Long idDocente;
+
+    @JsonProperty("comentario")
     @NotBlank
     private String comentario;
 
+    @JsonProperty("calificacion")
     @Min(1) @Max(10)
     private int calificacion;
 
     public CommentDto() {}
 
-    public Long getIdUsuario() { return id_usuario; }
-    public void setIdUsuario(Long id_usuario) { this.id_usuario = id_usuario; }
+    public Long getIdUsuario() { return idUsuario; }
+    public void setIdUsuario(Long idUsuario) { this.idUsuario = idUsuario; }
 
-    public Long getIdDocente() { return id_docente; }
-    public void setIdDocente(Long id_docente) { this.id_docente = id_docente; }
+    public Long getIdDocente() { return idDocente; }
+    public void setIdDocente(Long idDocente) { this.idDocente = idDocente; }
 
     public String getComentario() { return comentario; }
     public void setComentario(String comentario) { this.comentario = comentario; }
